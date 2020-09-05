16864d7efb3de77cdbbdedddb4c7de2bb11b6c22
richard1230 <yongjinhu1230@gmail.com>
2020-09-05 12:51:09 +0800

    deploy

---
+++ precache-manifest.1ac189db417b58c7122f1d231f086126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe48a7987f5e149d5db4c27469445a2",
+    "url": "/pocketBook-website/index.html"
+  },
+  {
+    "revision": "b45e3ae274c9d33a9973",
+    "url": "/pocketBook-website/static/css/main.da6129e6.chunk.css"
+  },
+  {
+    "revision": "786a990a2033be097a51",
+    "url": "/pocketBook-website/static/js/2.327db450.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/pocketBook-website/static/js/2.327db450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45e3ae274c9d33a9973",
+    "url": "/pocketBook-website/static/js/main.0d8b557c.chunk.js"
+  },
+  {
+    "revision": "d93083e11b928babb433",
+    "url": "/pocketBook-website/static/js/runtime-main.8a689cf0.js"
+  }
+]);
